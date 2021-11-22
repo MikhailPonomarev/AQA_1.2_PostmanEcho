07885989bfbafa06b6397b438e8701e040d6285a
MikhailPonomarev <michailp800@gmail.com>
2021-11-22 20:41:51 +0300

    Updated dependencies, changed CI config, test should fail

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -7,6 +7,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class PostmanEchoTest {
+
     @Test
     public void PostRequest() {
         given()
@@ -17,7 +18,7 @@ public class PostmanEchoTest {
                 .post("/post")
 
                 .then()
-                .statusCode(200)
+                .statusCode(500)
                 .contentType(ContentType.JSON)
                 .body("data", equalTo("test request"))
         ;
